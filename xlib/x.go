0def18fcac3cf55dc165a7a28cc7574e715c4e8e
启程 <qichengzx@gmail.com>
2018-02-13 13:49:16 +0800

    修改构造分页的方法

--- xlib/x.go
+++ xlib/x.go
@@ -30,23 +30,15 @@ var indexPage = "index.html"
 func (s *Site) Build() {
 	s.CurrentPage = "index"
 	count := len(s.Posts)
-	perPage := s.Cfg.PerPage
-	pageCount := float64(0)
 
-	if count > perPage {
-		pageCount = math.Ceil(float64(count) / float64(perPage))
-	}
-
-	var pn = PageNav{}
-	pn.PageCount = int(pageCount)
-	s.PageNav = pn.Handler()
 	s.CurrentPageIndex = 1
+	s.makePagnition(count, s.Cfg.PerPage)
 
 	// backup
 	var posts = s.Posts
-	if pn.PageCount > 1 {
+	if s.PageNav.PageCount > 1 {
 		s.NextPageIndex = 2
-		s.Posts = posts[:perPage]
+		s.Posts = posts[:s.Cfg.PerPage]
 	}
 
 	clearDir(s.Cfg.PublicDir)
@@ -54,9 +46,9 @@ func (s *Site) Build() {
 
 	makeFile(bt, filepath.Join(s.Cfg.PublicDir, indexPage))
 
-	if pageCount > 0 {
-		for i := perPage; i <= s.PageNav.PageCount; i++ {
-			s.Posts = posts[i*perPage-perPage : i*perPage]
+	if s.PageNav.PageCount > 0 {
+		for i := s.Cfg.PerPage; i <= s.PageNav.PageCount; i++ {
+			s.Posts = posts[i*s.Cfg.PerPage-s.Cfg.PerPage : i*s.Cfg.PerPage]
 			s.CurrentPageIndex = i
 			s.NextPageIndex = i + 1
 			s.PrevPageIndex = i - 1
@@ -67,6 +59,20 @@ func (s *Site) Build() {
 			makeFile(bt, filepath.Join(s.Cfg.PublicDir, s.Cfg.PaginationDir, p, indexPage))
 		}
 	}
+}
+
+func (s *Site) makePagnition(count int, perPage int) *Site {
+	pageCount := float64(0)
+
+	if count > perPage {
+		pageCount = math.Ceil(float64(count) / float64(perPage))
+	}
+
+	var pn = PageNav{}
+	pn.PageCount = int(pageCount)
+	s.PageNav = pn.Handler()
+
+	return s
 }
 
 func (s *Site) renderPage() []byte {

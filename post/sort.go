49a65b5d82527d80c94a9ac8fbf367a05f0ebd6a
启程 <qichengzx@gmail.com>
2018-02-22 14:49:03 +0800

    修改sort

--- post/sort.go
+++ post/sort.go
@@ -2,38 +2,16 @@ package post
 
 import (
 	"sort"
-	"time"
 )
 
-type PostWrapper struct {
-	post []Post
-	by   func(p, q *Post) bool
-}
+type postWrapper []Post
 
-type SortBy func(p, q *Post) bool
-
-func (pw PostWrapper) Len() int {
-	return len(pw.post)
-}
-
-func (pw PostWrapper) Swap(i, j int) {
-	pw.post[i], pw.post[j] = pw.post[j], pw.post[i]
-}
-
-func (pw PostWrapper) Less(i, j int) bool {
-	return pw.by(&pw.post[i], &pw.post[j])
-}
+func (pw postWrapper) Len() int           { return len(pw) }
+func (pw postWrapper) Less(i, j int) bool { return pw[i].Unixtime > pw[j].Unixtime }
+func (pw postWrapper) Swap(i, j int)      { pw[i], pw[j] = pw[j], pw[i] }
 
 func SortPost(posts []Post) []Post {
-	sort.Sort(PostWrapper{posts, func(p, q *Post) bool {
-		t1, _ := time.Parse("2006-01-02 15:04:05", p.Date)
-		t2, _ := time.Parse("2006-01-02 15:04:05", q.Date)
-
-		p.Unixtime = t1.Unix()
-		q.Unixtime = t2.Unix()
-
-		return q.Unixtime < p.Unixtime
-	}})
+	sort.Sort(postWrapper(posts))
 
 	return posts
 }

20e413b6913f7b421640fc47a9c8fd97e3a650c4
启程 <qichengzx@gmail.com>
2019-04-17 17:47:15 +0800

    修改入口参数，generate or serve

--- main.go
+++ main.go
@@ -1,25 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/qichengzx/gopress/generator"
 	"log"
+	"net/http"
+	"os"
 	"time"
 )
 
 var cfFile = "./_config.yaml"
 
 func main() {
-	timeStart := time.Now()
-	log.Println("Process begining")
-	defer func() {
-		timeEnd := time.Now()
-		used := timeEnd.Sub(timeStart)
-		log.Println("Process done")
-		log.Println("Used", used)
-	}()
+	generatePtr := flag.Bool("g", true, "Generate static files")
+	servePtr := flag.Bool("s", false, "Serve static files")
+	portPtr := flag.String("p", "8092", "Server port")
+	flag.Parse()
 
-	var site = generator.New(cfFile)
-	fmt.Println("Welcome to ", site.Cfg.Title)
-	site.Build()
+	if *generatePtr {
+		timeStart := time.Now()
+		log.Println("Process begining")
+		defer func() {
+			timeEnd := time.Now()
+			used := timeEnd.Sub(timeStart)
+			log.Println("Process done")
+			log.Println("Used", used)
+		}()
+
+		var site = generator.New(cfFile)
+		site.Build()
+		os.Exit(0)
+	}
+
+	if *servePtr && *portPtr != "" {
+		fmt.Println("server run at", *portPtr)
+		err := http.ListenAndServe(*portPtr, http.FileServer(http.Dir("./public/")))
+		if err != nil {
+			log.Fatal("ListenAndServe: ", err)
+		}
+	}
 }

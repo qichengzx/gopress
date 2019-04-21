17f796132e19812c7df8eaf308766c78cad39a6b
启程 <qichengzx@gmail.com>
2019-04-21 14:22:52 +0800

    fix flag

--- main.go
+++ main.go
@@ -6,19 +6,18 @@ import (
 	"github.com/qichengzx/gopress/generator"
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
-var cfFile = "./_config.yaml"
-
 func main() {
-	generatePtr := flag.Bool("g", true, "Generate static files")
+	generatePtr := flag.Bool("g", false, "Generate static files")
 	servePtr := flag.Bool("s", false, "Serve static files")
 	portPtr := flag.String("p", "8092", "Server port")
 	flag.Parse()
 
-	if *generatePtr {
+	switch {
+
+	case *generatePtr: 
 		timeStart := time.Now()
 		log.Println("Process begining")
 		defer func() {
@@ -28,16 +27,17 @@ func main() {
 			log.Println("Used", used)
 		}()
 
-		var site = generator.New(cfFile)
+		var site = generator.New("./config.yaml")
 		site.Build()
-		os.Exit(0)
-	}
+		break;
 
-	if *servePtr && *portPtr != "" {
+	case *servePtr && *portPtr != "" :
 		fmt.Println("server run at", *portPtr)
-		err := http.ListenAndServe(*portPtr, http.FileServer(http.Dir("./public/")))
+		err := http.ListenAndServe(":"+*portPtr, http.FileServer(http.Dir("./public/")))
 		if err != nil {
 			log.Fatal("ListenAndServe: ", err)
 		}
+	default:
+		flag.Usage()
 	}
 }
